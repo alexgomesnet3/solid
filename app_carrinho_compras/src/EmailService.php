f8929149b36cc72d522afe5f1f17acab099781c4
alexgomesnet3 <alex.gomes.net3@gmail.com>
2022-05-26 21:29:49 -0300

    Refactoring EmailService Class

--- app_carrinho_compras/src/EmailService.php
+++ app_carrinho_compras/src/EmailService.php
@@ -3,7 +3,28 @@
 namespace App;
 
 class EmailService {
-    public function teste() {
 
+    // attributes
+    private $from;
+    private $to;
+    private $title;
+    private $message;
+
+
+    // methods
+    public function __construct(
+        string $from = 'contato@email.com',
+        string $to = '',
+        string $title = '',
+        string $message = '',
+    ) {
+        $this->from = $from;
+        $this->to = $to;
+        $this->title = $title;
+        $this->message = $message;
+    }
+
+    public static function dispararEmail() {
+        return "E-mail disparado com sucesso!";
     }
 }
