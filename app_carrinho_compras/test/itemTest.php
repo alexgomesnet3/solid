06082f31f194140a00a94b3c45995f7257c6f77e
alexgomesnet3 <alex.gomes.net3@gmail.com>
2022-05-29 22:47:58 -0300

    Implement our first class test

---
+++ app_carrinho_compras/test/itemTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class itemTest extends TestCase {
+
+    public function testEstouFuncionando() {
+
+        $valor = 10;
+        $this->assertEquals($valor, 11);
+    }
+}
