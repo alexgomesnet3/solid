fde9d5cbb8251d0c345d8b19aff658fac3032150
alexgomesnet3 <alex.gomes.net3@gmail.com>
2022-06-07 22:08:40 -0300

    Add new Reader with OCP Principle

--- app_etl/index.php
+++ app_etl/index.php
@@ -17,6 +17,12 @@ $leitorCSV->setDiretorio(__DIR__.'/arquivos');
 $leitorCSV->setArquivo('dados.csv');
 $arr_csv = $leitorCSV->lerArquivo();
 
+//---------------------------  XLSX
+$leitorXLSX = new Leitor();
+$leitorXLSX->setDiretorio(__DIR__.'/arquivos');
+$leitorXLSX->setArquivo('dados.xlsx');
+$arr_xlsx = $leitorXLSX->lerArquivo();
+
 echo '<pre>';
-print_r(array_merge($arr_txt, $arr_csv));
+print_r(array_merge($arr_txt, $arr_csv, $arr_xlsx));
 echo '</pre>';
