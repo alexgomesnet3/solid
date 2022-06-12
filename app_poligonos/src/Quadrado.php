2ba9dc0e9b181535f74ae6ba776d1937c93f9fad
alexgomesnet3 <alex.gomes.net3@gmail.com>
2022-06-12 16:34:03 -0300

    Building classes Quadrado and Retangulo, without LSP

--- app_poligonos/src/Quadrado.php
+++ app_poligonos/src/Quadrado.php
@@ -1,6 +1,14 @@
 <?php
 
 namespace src;
-class Quadrado {
+class Quadrado extends Retangulo {
+    public function setLargura(float $largura): void {
+        $this->largura = $largura;
+        $this->altura = $largura;
+    }
 
+    public function setAltura(float $altura): void {
+        $this->altura = $altura;
+        $this->largura = $altura;
+    }
 }
