28d7a1b68c78e48b4801b99eab7af0e643da051d
alexgomesnet3 <alex.gomes.net3@gmail.com>
2022-06-12 16:53:36 -0300

    Refactoring Poligonos - Phase 1

--- app_poligonos/index.php
+++ app_poligonos/index.php
@@ -8,11 +8,15 @@ use src\Retangulo;
 $retangulo = new Retangulo();
 $retangulo->setAltura(5);
 $retangulo->setLargura(10);
-
 echo "<h3>Area do Retangulo: ".$retangulo->getArea()."</h3>";
 
 echo "</br'>";
 $quadrado = new Quadrado();
 $quadrado->setAltura(5);
-
 echo "<h3>Area do Quadrado: ".$quadrado->getArea()."</h3>";
+
+echo "</br'>";
+$retangulo = new Quadrado();
+$retangulo->setAltura(5);
+$retangulo->setLargura(10);
+echo "<h3>LSP - Area do Retangulo: ".$retangulo->getArea()."</h3>";
